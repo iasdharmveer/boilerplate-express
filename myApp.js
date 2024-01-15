5a6d215c141522a2c520f559217a9167827ca76f
Dharmveer Kumar <77826831+iasdharmveer@users.noreply.github.com>
2024-01-15 19:17:28 +0000

    added middleware logger

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
+const { configDotenv } = require('dotenv');
 let express = require('express');
+require("dotenv").config()
 let app = express();
+app.use((req, res, next) => {
+    const logMessage = `${req.method} ${req.path} - ${req.ip}`;
+    console.log(logMessage);
+    next();
+});
 console.log("Hello World");
 
 app.get("/", (req, res)=>{
@@ -7,7 +14,20 @@ app.get("/", (req, res)=>{
     res.sendFile(absolutePath);
 })
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public",express.static(__dirname + "/public"));
+
+app.get("/json", (req, res)=> {
+
+    const message= {"message": "Hello json"};
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        message.message=message.message.toUpperCase();
+
+    }
+    res.json(message);
+})
+app.get('/env', (req, res) => {
+    res.send(process.env.MESSAGE_STYLE);
+})
 
 
 
