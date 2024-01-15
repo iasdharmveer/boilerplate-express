4552cebd84dd3dd05d99e1c45ceb32df63e6edca
Dharmveer Kumar <77826831+iasdharmveer@users.noreply.github.com>
2024-01-15 11:21:19 +0000

    added css file using express static method

--- myApp.js
+++ myApp.js
@@ -2,8 +2,12 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+app.get("/", (req, res)=>{
+    const absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath);
+})
 
-
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
